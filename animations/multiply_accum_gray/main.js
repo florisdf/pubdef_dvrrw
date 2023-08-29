d95465e9fb4a954f7b3145540d9982eae6e975bb
Floris De Feyter <floris_defeyter@hotmail.com>
2023-08-29 22:00:30 +0200

    Add animations: channels slide over each other and form image

--- animations/multiply_accum_gray/main.js
+++ animations/multiply_accum_gray/main.js
@@ -8,7 +8,6 @@ import ghostIdxsBlue from './cyan_ghost_blue.js';
 import pacmanIdxsGray from './pacman_gray.js';
 
 import { Neuron } from './neuron.js';
-import { getColorTable, getColorSquare } from './pixel_tables.js';
 
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -17,13 +16,10 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 function getAnimationTimeline({
     neuron, scene
 }) {
-    const inputGroup = neuron.group.getObjectByName('input');
-    const productGroup = neuron.group.getObjectByName('productResult');
+    const inputGroup = neuron.inputGroup;
+    const productGroup = neuron.productGroup;
     const tableSize = neuron.tableSize;
     const cellSize = neuron.cellSize;
-
-    neuron.colorOpacity = 1.0;
-    neuron.numberOpacity = 0.0;
 
     [...neuron.productBoxes, ...neuron.equalsBoxes].forEach(box => {
         box.startWidth = cellSize;
@@ -53,16 +49,15 @@ function getAnimationTimeline({
     container.appendChild(renderEl);
     renderer.setSize(canvasWidth, canvasHeight);
 
-    neuron.channelMargin = - tableSize / 2;
-    const controls = new OrbitControls(camera, renderEl);
+    // const controls = new OrbitControls(camera, renderEl);
 
     function render() {
-        // [
-        //     ...neuron.productBoxes,
-        //     ...neuron.equalsBoxes,
-        //     neuron.sumBox,
-        //     neuron.actBox
-        // ].forEach(b => b.operand.lookAt(camera.position))
+        [
+            ...neuron.productBoxes,
+            ...neuron.equalsBoxes,
+            neuron.sumBox,
+            neuron.activationBox
+        ].forEach(b => b.operand.lookAt(camera.position))
         renderer.render(scene, camera); 
     }
 
@@ -76,78 +71,19 @@ function getAnimationTimeline({
     const tl = gsap.timeline({
         delay: 2,
         onUpdate: render,
-        onComplete: animateControl,
+        // onComplete: animateControl,
         defaults: {
             ease: "power2.inOut" 
         },
     });
 
-    /**
-    tl.from(inputMeshes.flat().flat().map(m => m.material), {
-        opacity: 0,
-    }).to(inputColorMeshes.flat().flat().map(m => m.material), {
-        opacity: 0,
-        delay: 2,
-    }, '<')
-
-    tl.from(camera.position, {
-        x: inputColorGroup.position.x + 2000,
-        y: inputColorGroup.position.y - tableSize / 2,
-        z: inputColorGroup.position.z - tableSize / 2,
-        duration: 2,
-        delay: 2,
-    }).from(camera.rotation, {
-        y: Math.PI / 2,
+    tl.to(neuron, {
+        channelMargin: -tableSize,
+        colorOpacity: 1.0,
+        numberOpacity: 0.0,
         duration: 2
-    }, '<')
+    });
 
-    tl.from(weightMeshes.flat().flat().map(m => m.material), {
-        opacity: 0,
-        delay: 2,
-    }).from(weightGroup.position, {
-        y: "-=100",
-    }, '<')
-
-    tl.from(opBoxTimes, {width: 0, duration: 2, delay: 2})
-        .from(opBoxTimes, {opSize: 0, duration: 2}, '<')
-        .from(opBoxEq, {width: 0, duration: 2})
-        .from(opBoxEq, {opSize: 0, duration: 2}, '<');
-    const opBoxPositions = [opBoxTimes.group.position, opBoxEq.group.position];
-    productMeshes.forEach(
-        (channel, c) => channel.forEach((row, i, arr) => {
-            const duration = 0.05;
-            row.forEach((mesh, j) => {
-                const dur = (i !== 0 || j > 5) ? duration : duration * 10;
-                if (j !== 0) {
-                    tl.to(opBoxPositions, {z: `-=${cellSize.x}`, duration: dur})
-                }
-                tl.from(mesh.material, {opacity: 0, duration: dur});
-            });
-            if (i < arr.length - 1) {
-                tl.to(opBoxPositions, {y: `-=${cellSize.y}`, z: `+=${(row.length - 1)*cellSize.x}`, duration: duration});
-            }
-            tl.to(opBoxPositions, {y: `-=${tableSize}`, z: `+=${shiftX}`});
-        })
-    );
-    tl.to([opBoxTimes, opBoxEq], {width: 0});
-
-    tl.from(opBoxSum, {heightEnd: opBoxSum.heightStart, width: 0, duration: 2}, '<')
-        .from(opBoxSum, {opSize: 0, duration: 2}, '<')
-        .from(macMesh.material, {
-            opacity: 0,
-        }).to(camera.rotation, {
-            y: Math.PI/2,
-            yoyo: true,
-            repeat: 1,
-            repeatDelay: 3,
-        }).to(camera.position, {
-            x: macMesh.position.x + 400,
-            z: macMesh.position.z,
-            yoyo: true,
-            repeat: 1,
-            repeatDelay: 3,
-        }, '<');
-    */
     return tl;
 }
 
@@ -166,27 +102,6 @@ function getGhostSceneComps(input, weights) {
 
     const scene = new THREE.Scene();
     scene.add(neuron.group);
-
-    const inputImageGroup = getColorTable({
-        colors: input[0].map((row, i) => row.map((x, j) => {
-                if (neuron.numChannels === 1) {
-                    x = Math.round(x*255);
-                    return `rgb(${x}, ${x}, ${x})`;
-                } else if (neuron.numChannels === 3) {
-                    const r = Math.round(x*255);
-                    const g = Math.round(input[1][i][j] * 255);
-                    const b = Math.round(input[2][i][j] * 255);
-                    return `rgb(${r}, ${g}, ${b})`;
-                }
-            })),
-        cellSize: neuron.cellSize,
-    });
-    const midChannel = Math.floor(neuron.numChannels / 2);
-    const inputGroup = neuron.group.getObjectByName('input');
-    inputImageGroup.position.x = inputGroup.children[midChannel].position.x
-    inputImageGroup.name = "inputImage";
-    // scene.add(inputImageGroup);
-
 
     const gridHelper = new THREE.GridHelper(1000, 10);
     const axesHelper = new THREE.AxesHelper(2000);
